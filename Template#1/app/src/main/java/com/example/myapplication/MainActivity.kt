f071299102e18fd42400f0fc6b06133365cc8950
Pavlo Prokopenko <stpasha6@gmail.com>
2023-10-12 22:21:18 +0300

    Learned how to change something by code

--- Template#1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Template#1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,42 +3,17 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MyLogMA", "onCreate")
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("MyLogMA", "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("MyLogMA", "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("MyLogMA", "onPause")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("MyLogMA", "onDestroy")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("MyLogMA", "onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("MyLogMA", "onRestart")
-
+    fun onClickListener(view : View) {
+        val textViewTest = findViewById<TextView>(R.id.textViewTest)
+        textViewTest.text = "Now I know how to make change text by button"
     }
 }

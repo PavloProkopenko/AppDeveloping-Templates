082f7f8520a5845fcbd8d7a8de868bd4cea4df44
Pavlo Prokopenko <stpasha6@gmail.com>
2023-10-13 23:29:58 +0300

    Theoretical dat
    Today I listend a lot of theory of Kotlin, so I don't write a lot of code. Tomorrow I will have practiacl day, so my code will be larger than now

--- Template#1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Template#1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,13 +7,17 @@ import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    // Global variables (can use in any function of class)
+    var globalVariable1 = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun onClickListener(view : View) {
+        // local variable (can use only in this method)
         val textViewTest = findViewById<TextView>(R.id.textViewTest)
         textViewTest.text = "Now I know how to make change text by button"
+        globalVariable1 = 2 // we can do this, because this is global variable
     }
 }

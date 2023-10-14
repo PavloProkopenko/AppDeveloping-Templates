3262cd775f7b65c24209cd6fbf780801f5719ba1
Pavlo Prokopenko <stpasha6@gmail.com>
2023-10-14 21:48:39 +0300

    IF and WHEN usage
    In this app I coded two variants of simple program which decide what skill level you have. In my opinion, using WHEN is more comfortable than IF, so I commented code with IF block, so you can see how it works

--- Template#1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Template#1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,36 +9,51 @@ import android.widget.TextView
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bindingClass : ActivityMainBinding
-    private val number1 = 324
-    private val number2 = 34
+    private lateinit var bindingClass: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
-        bindingClass.button2.setOnClickListener {
-            val result = number1 + number2
-            bindingClass.Result.text = "Result of 324 + 34: $result"
+
+
+        bindingClass.button.setOnClickListener {
+            val experience: Int = bindingClass.amountOfYears.text.toString().toInt()
+
+//            Variant with IF
+//            if (experience < 1) {
+//                bindingClass.result.text = "You are a Trainee"
+//            }
+//            if (experience in 1..2) {
+//                bindingClass.result.text = "You are a Junior"
+//            }
+//            if (experience in 3..5) {
+//                bindingClass.result.text = "You are a Middle"
+//            }
+//            if (experience in 5..8) {
+//                bindingClass.result.text = "You are a Senior"
+//            }
+
+//            Variant with WHEN
+            when(experience) {
+                in 0 ..1 -> {
+                    bindingClass.result.text = "You are a Trainee"
+                }
+                in 1..2 -> {
+                    bindingClass.result.text = "You are a Junior"
+                }
+                in 2..5 -> {
+                    bindingClass.result.text = "You are a Middle"
+                }
+                in 5..8 -> {
+                    bindingClass.result.text = "You are a Senior"
+                }
+                else -> {
+                    bindingClass.result.text = "You are Team Lead or higher"
+                }
+            }
         }
 
-        bindingClass.button3.setOnClickListener {
-            val result = number1 - number2
-            bindingClass.Result.text = "Result of 324 - 24: $result"
-        }
 
-        bindingClass.button4.setOnClickListener {
-            val result = number1 * number2
-            bindingClass.Result.text = "Result of 324 * 24: $result"
-        }
-
-        bindingClass.button1.setOnClickListener {
-            val result = number1 / number2
-            bindingClass.Result.text = "Result of 324 / 24: $result"
-        }
     }
-
-
-
-
 }

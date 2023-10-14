addc79cf22c4bda6ba11abf22de7a24d2be5732e
Pavlo Prokopenko <stpasha6@gmail.com>
2023-10-14 12:55:07 +0300

    Simple calculator
    After lesson, I developed a simple calulator which do operations with numbers 324 and 34. It's not a big deal, but great practice at start. Next, I will watch video about using if and when and do some change in this app.

--- Template#1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Template#1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,21 +3,42 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.TextView
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    // Global variables (can use in any function of class)
-    var globalVariable1 = 1
+    private lateinit var bindingClass : ActivityMainBinding
+    private val number1 = 324
+    private val number2 = 34
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+        bindingClass.button2.setOnClickListener {
+            val result = number1 + number2
+            bindingClass.Result.text = "Result of 324 + 34: $result"
+        }
+
+        bindingClass.button3.setOnClickListener {
+            val result = number1 - number2
+            bindingClass.Result.text = "Result of 324 - 24: $result"
+        }
+
+        bindingClass.button4.setOnClickListener {
+            val result = number1 * number2
+            bindingClass.Result.text = "Result of 324 * 24: $result"
+        }
+
+        bindingClass.button1.setOnClickListener {
+            val result = number1 / number2
+            bindingClass.Result.text = "Result of 324 / 24: $result"
+        }
     }
 
-    fun onClickListener(view : View) {
-        // local variable (can use only in this method)
-        val textViewTest = findViewById<TextView>(R.id.textViewTest)
-        textViewTest.text = "Now I know how to make change text by button"
-        globalVariable1 = 2 // we can do this, because this is global variable
-    }
+
+
+
 }
